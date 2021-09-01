3e9273dafef515c6f9b5f6f39a4b969f4fb92be6
DGravenb <you@example.com>
2021-09-01 07:28:20 +0200

    layers

--- connect.js
+++ connect.js
@@ -1,5 +1,4 @@
 import { useWeb3React } from "@web3-react/core"
-import { injected } from "../components/wallet/Connectors"
 
 export default function Home() {
   const { active, account, library, connector, activate, deactivate } = useWeb3React()
